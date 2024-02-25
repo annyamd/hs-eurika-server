b228dc1599c77e635020ab89d3ed2d49e4527633
annyamd <miann9920@gmail.com>
2024-02-25 23:51:35 +0300

    Initial commit

---
+++ src/test/java/com/example/hseurikaserver/HsEurikaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hseurikaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsEurikaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
